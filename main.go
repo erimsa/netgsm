c6bf45d157063dc86264aeac19a30955a558625a
erimsa <erimsalih@gmail.com>
2021-09-29 03:16:04 +0300

    Netgsm SMS Otp request function added

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 
-	netgsm "github.com/ozgur-soft/netgsm/src"
+	netgsm "github.com/erimsa/netgsm/src"
 )
 
 func main() {
-	config := netgsm.Config{SmsCompany: "NETGSM", SmsMsgHeader: "", SmsUserCode: "", SmsPassword: "", ApiUrl: "https://api.netgsm.com.tr/sms/send/xml"}
+	config := netgsm.Config{SmsCompany: "NETGSM", SmsMsgHeader: "", SmsUserCode: "", SmsPassword: "", ApiUrl: "https://api.netgsm.com.tr/sms/send/otp"}
 	api := &netgsm.API{config}
 	request := netgsm.Request{}
 	request.MainBody.Body.Msg = "test"
 	request.MainBody.Body.No = "905555555555"
-	send := api.Sms(request)
+	send, otp := api.Sms_Otp(request)
 	if send {
-		fmt.Println("mesaj iletildi")
+		fmt.Println("mesaj gönderildi.")
+		fmt.Println("returned otp : " + otp)
 	} else {
 		fmt.Println("hata oluştu")
 	}
--- src/netgsm.go
+++ src/netgsm.go
@@ -2,6 +2,8 @@ package netgsm
 
 import (
 	"encoding/xml"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -37,6 +39,15 @@ type Request struct {
 	} `xml:"mainbody,omitempty"`
 }
 
+type Responce struct {
+	XMLName xml.Name `xml:"xml,omitempty"`
+	Main    struct {
+		Code  string `xml:"code,omitempty"`
+		JobID string `xml:"jobID,omitempty"`
+		Error string `xml:"error,omitempty"`
+	} `xml:"main,omitempty"`
+}
+
 func (api *API) Sms(request Request) bool {
 	request.MainBody.Header.Company = api.Config.SmsCompany
 	request.MainBody.Header.MsgHeader = api.Config.SmsMsgHeader
@@ -55,4 +66,35 @@ func (api *API) Sms(request Request) bool {
 	}
 	defer res.Body.Close()
 	return true
+}
+
+func (api *API) Sms_Otp(request Request) (bool, string) {
+	request.MainBody.Header.UserCode = api.Config.SmsUserCode
+	request.MainBody.Header.Password = api.Config.SmsPassword
+	request.MainBody.Header.MsgHeader = api.Config.SmsMsgHeader
+	request.MainBody.Body.Msg = "<![CDATA[" + request.MainBody.Body.Msg + " - ]]>"
+	postdata, err := xml.Marshal(request)
+	if err != nil {
+		return false, ""
+	}
+	rpl := strings.NewReplacer("&lt;!", "<!", "]&gt;", "]>", "<xml>", "", "</xml>", "")
+	res, err := http.Post(api.Config.ApiUrl, "text/xml; charset=utf-8", strings.NewReader(xml.Header+rpl.Replace(string(postdata))))
+	if err != nil {
+		return false, ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return false, ""
+	}
+	fmt.Println(body)
+	var resp Responce
+	xml.Unmarshal(body, &resp)
+	fmt.Println(resp)
+	if resp.Main.Code != "0" {
+		return false, ""
+	}
+
+	return true, resp.Main.JobID
 }
